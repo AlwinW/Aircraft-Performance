29291816c28eb382b5ac33b7d2875b1a635a93b0
Alwin <alwang18@hotmail.com>
2016-08-12 12:21:46 +1000

    Secant Root Finding
    Attemps not working. Changing to for loop instead

--- Helper Functions.R
+++ Helper Functions.R
@@ -47,28 +47,64 @@ V32 <- function(Vstar) (1/3)^(1/4) * Vstar
 
 #--- Power Required
 # Minimum power i.e. (L^(3/2)/D)max @ V32
-PRmin <- function(rho, W, S, Cd0, K) (256/27)^0.25 * (2/rho * W/S)^0.5 * (Cd0*K^3)^0.25
+PRmin <- function(rho, W, S, Cd0, K) (256/27)^0.25 * (2/rho * W/S)^0.5 * (Cd0*K^3)^0.25 * W
 # Power required @ any airspeed
 PR <- function(W, ClCd, Vinf) W/ClCd * Vinf
 
+#--- Thrust Required
+TRmin <- function(W, ClCdstar) W/ClCdstar
+TR <- function(W, ClCd) W / ClCd
+
+#--- Altitude Effect
+# Altitude constants
+alt_r = 0.5; alt_s = 0.7
+# Power Available
+PA <- function(sigma, P0) sigma^0.7 * P0
+# Excess Power
+Pexc <- function(PA, PR) PA - PR
+# Thrust Available
+TA <- function(PA, Vinf) PA / Vinf
+# Excess Thrust
+Texc <- function(TA, TR) TA - TR
+
+#---Maximum speed @ altitude
+Vmax <- function(PA, rho, W, S, Cd0, K, x1, x2) {
+  SecantRootUnivariate(function(Vinf) PA - W * sqrt(2/rho * W/S *
+                                              (Cd0 + K*(2/rho*W/S*1/(Vinf^2))^2)^2/((2/rho*W/S*1/(Vinf^2))^3)), x1, x2)
+}
+
+Vmax(3060e3, 1.225, 155e3, 54.4, 0.02, 0.323, 100, 200)
 
 # Dummy power in level flight curve
 nh <- 6; nv <- 51
 test  <- data.frame(h = rep(seq(0,5000,length.out=nh),each=nv), Vinf = rep(seq(40,120,length.out=nv),times=nh))
 test <- test %>%
-  mutate(W = 115e3, S = 54.4, K = 0.0323, Cd0 = 0.02) %>%
+  mutate(W = 115e3, S = 54.4, K = 0.0323, Cd0 = 0.02, P0 = 3060e3) %>%
   StandardAtomsphere(.) %>%
   group_by(h) %>%
   mutate(qinf = qinf(rho, Vinf),
     Cl = Cl(W, qinf, S),
     Cd = Cd(Cd0, K, Cl),
     ClCd = ClCd(Cl, Cd),
+    ClCdstar = ClCdstar(Cd0, K),
+    PRmin = PRmin(rho, W, S, Cd0, K),
     PR = PR(W, ClCd, Vinf),
+    TRmin = TRmin(W, ClCdstar),
+    TR = TR(W, ClCd),
     Vstar = Vstar(rho, W, S, K, Cd0),
-    V32 = V32(Vstar))
+    V32 = V32(Vstar),
+    PA = PA(sigma, P0)
+    )
+# POWER
 ggplot(test, aes(x=Vinf, y=PR, group=h, colour=as.factor(h))) +
   geom_path() +
-  geom_point(aes(x=Vstar, y=0), shape = 1) +
-  geom_point(aes(x=V32, y=0), shape = 2) +
+  geom_point(aes(x=V32, y=PRmin), shape = 1) +
+  geom_point(aes(x=Vstar, y=TRmin*Vstar), shape = 2) +
+  expand_limits(x = 0, y = 0)
+# THRUST
+ggplot(test, aes(x=Vinf, y=TR, group=h, colour=as.factor(h))) +
+  geom_path() +
+  geom_point(aes(x=V32, y=PRmin/V32), shape = 1) +
+  geom_point(aes(x=Vstar, y=TRmin), shape = 2) +
   expand_limits(x = 0, y = 0)
 #---Use the previous functions to mutate a dataframe
---
+++ Helper Numerical Methods.R
@@ -0,0 +1,22 @@
+#----------------------------
+#--- Numerical Methods
+#============================
+
+#---Secant Root Finding Method
+
+SecantRootUnivariate <- function(func, x1, x2) {
+  # reduce number of function calls by storing results as vars
+  fx1 <-  func(x1); fx2 <- func(x2)
+  fxr <- 10; loop = 1
+  while (abs(fxr) > 0.00001 & loop < 50) {
+    xr <- x2 - (fx2*(x1 - x2))/(fx1 - fx2)
+    fxr <- func(xr)
+    if (fx1 * fxr < 0) {
+      x2 <- xr; fx2 <-  fxr
+    } else {
+      x1 <- xr; fx1 <- fxr
+    }
+    loop <- loop + 1
+  }
+  return(xr)
+}
