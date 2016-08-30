792fc53d790ab67782c49545477388f79a220040
Alwin <alwang18@hotmail.com>
2016-08-30 21:17:57 +1000

    Iteration Begun
    Note: Modified Secant method working for Clhls, BUT not needed. Will delete

---
+++ Aircraft Performance Iteration App/Helper Iteration.R
@@ -0,0 +1,152 @@
+#----------------------------
+#--- Main Iteration Solver
+#============================
+# This is the main iteration method in order to determine values
+
+## Initialise  ======================================================================
+
+input_initial <- data.frame(
+  S = 24.5, b = 22.1359, 
+  AR = 20, e = 0.8, K = 0.01989, Cd0 = 0.015,
+  Clclean = 1.5, Clflaps = 1.2, Clhls = 1.5,
+  m = 6500, W = 63742, WS = 2601.72,
+  P0eng = 180000, P0 = 360000, Etatotal = 0.80, alt_s = 0, 
+  ClG = 0.25, Cd0G = 0.025, hground = 2.5
+)
+
+inputvals <- input_initial
+
+specifications <- data.frame(
+  Variable = c("Mp", "Wp", "E", "Dens", "Srun", "Hobs", "Vappmax", "PerGrad2Seg", "ClimbCruise", "ClimbCeil", "AltCruise", "AltCeil", 
+               "Mach", "Range", "LoadMax", "LoadMin"),
+  Description = c("Payload Mass (kg)","Payload Weight (N)","Battery Specific Energy (J/kg)","Battery Density (kg/m^3)",
+                  "Runway Length (m)","Screen Height (m)","Maximum Landing Speed (m/s)","Climb Gradient 2nd Segment (%)",
+                  "Climb at Cruise (m/s)","Climb at Ceiling (m/s)","Altitude of Cruise (m)","Altitude of Ceiling (m)",
+                  "Cruise Mach Number (M)","Range (m)","Load Factor Max (n)","Load Factor Min (n)"),
+  Value = c(120 * 6, 120 * 6 * 9.8065, 1e6, 2750, 1200, 35 * 0.3048, 100 * 0.5144, 1.5, 300 * 0.3048 / 60,
+            100 * 0.3048 / 60, 10000 * 0.3048, 12000 * 0.3048, 0.25, 1000e3, 3.5, -1.5)
+)
+
+## Small Functions ======================================================================
+#--- Minimum velocity for some density, wing loading and max Cl
+Vmin <- function(rho, WS, Clmax) 
+  sqrt(2/rho * WS * 1/Clmax)
+
+#--- Power derating function
+PA <- function(P0, sigma)
+  P0 * sigma^inputvals$alt_s
+
+#--- Effective K due to ground effect
+Keff <- function(K, h, b)
+  (33 * (h/b)^1.5) / (1 + 33 * (h/b)^1.5)  * K
+
+#--- Values of mu for the friction on a runway
+groundmu <- data.frame(names = c("Dry Concrete", "Wet Concrete", "Icy Concrete"),
+                       brakesoff = c(0.04, 0.05, 0.02), #NB: 0.03-0.05 for dry
+                       brakeson_min = c(0.3, 0.15, 0.06),
+                       brakeson_max = c(0.5, 0.3, 0.10)) %>%
+  mutate(brakeson = (brakeson_min + brakeson_max)/2)
+
+#--- Function to determine the climb rates for a given power and configuration
+ClimbRatesFunction <- function(P, Cd0, rho, V, S, K, W) {
+  # Coefficients to simplify the calculation
+  a = P - 1/2 * Cd0 * rho * V^3 * S
+  b = (2 * K * W^2) / (rho * V * S)
+  c = W * V
+  # Solution to the quadratic
+  sintheta = c((c - sqrt(-4*a*b + 4*b^2 + c^2)) / (2*b),
+               (c + sqrt(-4*a*b + 4*b^2 + c^2)) / (2*b))
+  # Determine which angle to accept
+  sintheta = sintheta[sintheta < 0.5 & sintheta > -0.5]
+  # Return a meaningful result
+  if (length(sintheta) != 1) {
+    return(data.frame(Theta = NA, SinTheta = NA, PerGrad = NA, ClimbRate = NA))
+  }
+  else {
+    theta = asin(sintheta)
+    return(data.frame(
+      Theta = theta * 180 / pi, 
+      SinTheta = sintheta, 
+      PerGrad = tan(theta)*100, 
+      ClimbRate = sintheta * V))
+  }
+}
+
+#--- Update the other parameters based on m, e, etc (functionalise)
+UpdateParams <- function(input) {
+  input$b <- sqrt(input$AR * input$S)
+  input$K <- 1/(pi * input$AR * input$e)
+  input$W <- input$m * 9.8065
+  input$WS <- input$W / input$S
+  input$P0 <- input$P0eng * 2
+  return(input)
+}
+
+## Begin Calculations ======================================================================
+#--- Manipulate the data into a meaningful form
+inp  <- t(specifications["Value"])
+colnames(inp) <- t(specifications["Variable"])
+inp <- cbind(inputvals, inp)
+
+#--- Variables to be applied
+var_m <- seq(6000, 6500, 500)
+var_e <- seq(0.8, 0.85, 0.05)
+var_Cd0 <-  seq(0.015, 0.020, 0.005)
+var_WS <-  seq(1800, 2200, 200)
+
+#--- Apply the variables and update the dataframe
+iterationvals <- inp %>%
+  rowwise() %>%
+  do(data.frame(., var_m = var_m)) %>%
+  do(data.frame(., var_e = var_e)) %>%
+  do(data.frame(., var_Cd0 = var_Cd0)) %>%
+  do(data.frame(., var_WS = var_WS)) %>%
+  ungroup() 
+iterationvals  <- data.frame(iterationvals) %>%
+  mutate(m = var_m, e = var_e, Cd0 = var_Cd0, WS = var_WS) %>%
+  mutate(S = m*9.8065 / var_WS) %>%
+  select(-var_m, -var_e, -var_Cd0, -var_WS) %>%
+  UpdateParams(.)
+
+## For Loop ======================================================================
+for (i in 1:nrow(iterationvals))  {
+  
+  iv0 <- iterationvals[i,]
+  
+  
+## Determine Clhls from Vapp ======================================================================
+  #--- Initialise the while loop
+  Clhls <- iv0 %>%
+    mutate(h = 50*0.3048) %>%
+    StandardAtomsphere(.) %>%
+    select(Clclean, Clhls, rho, WS, Vappmax)
+  fr <- Clhls %>%
+    mutate(fx = 10)
+  xr <- 1
+  del <- 0.01
+  #--- Modified Secant Method (NB: Could have solved analytically lol)
+  while (abs(fr$fx) > 0.001 | fr$fx < 0) {
+    Clhls0 <- mutate(Clhls, 
+                     Clmax = Clclean + xr, 
+                     Vapp = 1.3 * Vmin(rho, WS, Clmax),
+                     fx = Vappmax - Vapp)
+    Clhls1 <- mutate(Clhls, 
+                     Clmax = Clclean + xr + del, 
+                     Vapp = 1.3 * Vmin(rho, WS, Clmax),
+                     fx = Vappmax - Vapp)
+    xr <- xr - (del * Clhls0$fx) / (Clhls1$fx - Clhls0$fx)
+    fr <- mutate(Clhls, 
+                 Clmax = Clclean + xr, 
+                 Vapp = 1.3 * Vmin(rho, WS, Clmax),
+                 fx = Vappmax - Vapp)
+  }
+  
+  #--- Check if Clhls is reasonable
+  if (xr < 0) xr = 0
+  #--- Return the result
+  iv0$Clhls <- xr
+  
+}
+
+
+
